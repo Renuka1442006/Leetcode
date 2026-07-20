3cf4e9b05cb9f9de0d09dce6a24c24eb2fd9db2a
Renuka1442006 <renuka1442006@gmail.com>
2026-07-20 18:46:51 +0530

    Time: 2 ms (94.54%), Space: 47.7 MB (49.98%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int n=nums.length;
+        /*int n=nums.length;
         int dup=0;
         int mis=0;
         //Arrays.sort(nums);
@@ -22,6 +22,27 @@ class Solution {
             actualsum+=num;
         }
         mis=dup-(actualsum-totalsum);
+        return new int[]{dup,mis};
+        */
+        int n=nums.length;
+        int[] freq=new int[n+1];
+        for(int num:nums)
+        {
+            freq[num]++;
+        }
+        int mis=0;
+        int dup=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(freq[i]==0)
+            {
+                mis=i;
+            }
+            if(freq[i]==2)
+            {
+                dup=i;
+            }
+        }
         return new int[]{dup,mis};
     }
 }
