6bb178926b268ce772c1613735c633093987506b
Renuka Khune <renuka1442006@gmail.com>
2026-08-29 13:17:17 +0530

    Time: 1291 ms (62.3%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-SELECT u2.unique_id, u1.name FROM Employees u1
-LEFT JOIN EmployeeUNI u2
-ON u1.id=u2.id;
+select euni.unique_id,e.name
+FROM Employees e 
+LEFT JOIN EmployeeUNI euni
+ON e.id=euni.id;
