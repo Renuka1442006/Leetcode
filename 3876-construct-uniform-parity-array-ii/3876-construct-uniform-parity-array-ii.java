84d4babb8d8ddcabde1d49e2ff5dc2eaa5cb2650
Renuka Khune <renuka1442006@gmail.com>
2026-09-03 19:50:59 +0530

    Time: 8 ms (50%), Space: 121.7 MB (44.2%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -4,21 +4,16 @@ class Solution {
 
         int minOdd = Integer.MAX_VALUE;
 
-        // Find the smallest odd number
         for (int x : nums1) {
             if (x % 2 == 1) {
                 minOdd = Math.min(minOdd, x);
             }
         }
 
-        // If there is no odd number,
-        // all numbers are even.
         if (minOdd == Integer.MAX_VALUE) {
             return true;
         }
 
-        // If an even number is smaller than
-        // the smallest odd number, answer is false.
         for (int x : nums1) {
             if (x % 2 == 0 && x < minOdd) {
                 return false;
